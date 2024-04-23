a622a5a1e63eb2f136f9e8d4e66c5761a346976f
AlexanderClausen <git@alexanderclausen.com>
2024-04-24 00:50:50 +1000

    remove test page

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'functions.php';
-    // echo getProductDetails(1);
-    // echo '';
-    // echo getBreadcrumbs(5, 'product');
-    // echo getCategory(27, 'category', 'products', false);
-    echo getAllCategories(true);
-    // echo getProduct(1);
